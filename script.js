b9b4096d43c408feb947a17f7df9f33aa8f660cb
ThimbaDM <timo.huuskonen1@gmail.com>
2025-02-23 00:22:12 +0200

    Popupin aukeaminen ajastetusti
    Toiminnallisuus aukaisulle. Hakee databasesta kaikki popupit konsoliin

--- script.js
+++ script.js
@@ -16,4 +16,24 @@ document.addEventListener("DOMContentLoaded", function () {
 //Printtaa konsolille annetun tekstin
 function afterTimeout() {
     console.log("5s on kulunut!")
+
+    // Demo siitä, miten popup aukeaa ajastetusti
+    
+    document.getElementById('overlay').style.display = 'block';
+    document.getElementById('popup').style.display = 'block';
+
+    printPopups();
+
+    //let pvm = document.getElementById("startDate").value;
+    //console.log("'"+pvm+"'");
 }
+
+function printPopups() {
+    fetch('/api/popups')
+        .then(response => response.json())
+        .then(data => {
+            const popupList = document.getElementById('popups');
+            //let parsedLst = JSON.parse(popupList);
+            console.log(popupList);
+        })
+    };
